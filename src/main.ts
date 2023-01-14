a4b37f5a1e8d3f3a2ec6e313b69220d9742472ec
hk-p114514 <hukada.gogotea@gmail.com>
2023-01-15 00:21:12 +0900

    test motion event

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { setupCounter } from "./counter";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -18,6 +18,20 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+const motion = document.getElementById("motion");
+
+function handleMotionEvent(event: DeviceMotionEvent) {
+  const x = event.accelerationIncludingGravity?.x;
+  const y = event.accelerationIncludingGravity?.y;
+  const z = event.accelerationIncludingGravity?.z;
+
+  if (motion) {
+    motion.innerText = `${x} : ${y} : ${z}`;
+  }
+}
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
