ebf0de8599499a9dae2cfae3801dfb25668c45b9
hk-p114514 <hukada.gogotea@gmail.com>
2023-01-22 03:08:48 +0900

    ビルド設定を完了

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-const root = resolve(__dirname, "src");
+const root = resolve(__dirname, "./");
+const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
   root,
   build: {
     outDir,
     rollupOptions: {
-      input: {},
+      input: {
+        gyro: resolve(root, "src", "gyro", "index.html"),
+        tetris: resolve(root, "src", "tetris", "index.html"),
+        // "/": resolve(root, "index.html"),
+      },
     },
   },
 });
