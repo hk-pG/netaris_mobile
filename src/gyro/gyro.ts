4b089a0cb355b27362399a07312105598f9eb33a
hk-p114514 <hukada.gogotea@gmail.com>
2023-01-22 22:29:53 +0900

    biggest

--- src/gyro/gyro.ts
+++ src/gyro/gyro.ts
@@ -11,13 +11,15 @@ motion?.appendChild(img);
 window.addEventListener("load", () => {
   window.addEventListener("deviceorientation", (e) => {
     // const a = e.absolute;
-    const z = e.alpha;
-    const x = e.beta;
-    const y = e.gamma;
+    const z = e.alpha!;
+    const x = e.beta!;
+    const y = e.gamma!;
 
     if (!motion) return;
     motion.innerHTML =
       //
-      `z: ${z?.toFixed(3)} x: ${x?.toFixed(3)} y: ${y?.toFixed(3)}`;
+      `z: ${z?.toFixed(3)} x: ${x?.toFixed(3)} y: ${y?.toFixed(
+        3
+      )}<br>biggest: ${Math.max(x, y, z)}`;
   });
 });
